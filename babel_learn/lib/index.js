30b51f97876b2be2aa9a8c426e82ced344e69060
董晶 <dongjing@cvte.com>
2020-10-31 11:58:19 +0800

    runtime-corejs3 实现避免全局污染

--- babel_learn/lib/index.js
+++ babel_learn/lib/index.js
@@ -1,25 +1,14 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault");
 
-var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+var _promise = _interopRequireDefault(require("@babel/runtime-corejs3/core-js-stable/promise"));
 
-var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
+var _includes = _interopRequireDefault(require("@babel/runtime-corejs3/core-js-stable/instance/includes"));
 
-var Point = /*#__PURE__*/function () {
-  function Point(x, y) {
-    (0, _classCallCheck2["default"])(this, Point);
-    this.x = x;
-    this.y = y;
-  }
+var _context;
 
-  (0, _createClass2["default"])(Point, [{
-    key: "getX",
-    value: function getX() {
-      return this.x;
-    }
-  }]);
-  return Point;
-}();
-
-var cp = new ColorPoint(25, 8);
+var isHas = (0, _includes["default"])(_context = [1, 2, 3]).call(_context, 2);
+new _promise["default"](function (resolve, reject) {
+  resolve(100);
+});

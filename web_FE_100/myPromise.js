a323620112110c35bd1d34ab1ad72937a268a316
wustdong <1051987765@qq.com>
2024-10-08 11:49:11 +0800

    ::fix:: 解决 executor 为异步函数时，执行顺序问题;

--- web_FE_100/myPromise.js
+++ web_FE_100/myPromise.js
@@ -5,41 +5,39 @@ const REJECTED = 'REJECTED';
 
 class Promise {
   constructor(executor) {
-    // 默认状态为 PENDING
     this.status = PENDING;
-    // 存放成功状态的值，默认为 undefined
     this.value = undefined;
-    // 存放失败状态的值，默认为 undefined
     this.reason = undefined;
+    // 存放成功的回调
+    this.onResolvedCallbacks = [];
+    // 存放失败的回调
+    this.onRejectedCallbacks= [];
 
-    // 调用此方法就是成功
     let resolve = (value) => {
-      // 状态为 PENDING 时才可以更新状态，防止 executor 中调用了两次 resovle/reject 方法
       if(this.status ===  PENDING) {
         this.status = FULFILLED;
         this.value = value;
+        // 依次将对应的函数执行
+        this.onResolvedCallbacks.forEach(fn=>fn());
       }
     } 
 
-    // 调用此方法就是失败
     let reject = (reason) => {
-      // 状态为 PENDING 时才可以更新状态，防止 executor 中调用了两次 resovle/reject 方法
       if(this.status ===  PENDING) {
         this.status = REJECTED;
         this.reason = reason;
+        // 依次将对应的函数执行
+        this.onRejectedCallbacks.forEach(fn=>fn());
       }
     }
 
     try {
-      // 立即执行，将 resolve 和 reject 函数传给使用者  
       executor(resolve,reject)
     } catch (error) {
-      // 发生异常时执行失败逻辑
       reject(error)
     }
   }
 
-  // 包含一个 then 方法，并接收两个参数 onFulfilled、onRejected
   then(onFulfilled, onRejected) {
     if (this.status === FULFILLED) {
       onFulfilled(this.value)
@@ -48,8 +46,21 @@ class Promise {
     if (this.status === REJECTED) {
       onRejected(this.reason)
     }
+
+    if (this.status === PENDING) {
+      // 如果promise的状态是 pending，需要将 onFulfilled 和 onRejected 函数存放起来，等待状态确定后，再依次将对应的函数执行
+      this.onResolvedCallbacks.push(() => {
+        onFulfilled(this.value)
+      });
+
+      // 如果promise的状态是 pending，需要将 onFulfilled 和 onRejected 函数存放起来，等待状态确定后，再依次将对应的函数执行
+      this.onRejectedCallbacks.push(()=> {
+        onRejected(this.reason);
+      })
+    }
   }
 }
+
 // const promise1 = new Promise((resolve, reject) => {
 //     resolve('成功');
 //   }).then(
@@ -76,3 +87,8 @@ class Promise {
     }
   )
     
+/**
+ * https://juejin.cn/post/6994594642280857630
+ * 
+ * https://juejin.cn/post/6850037281206566919
+ */

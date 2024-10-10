6bce61b787a595a40ff5833a58e55a50b1edc9bb
wustdong <1051987765@qq.com>
2024-10-10 09:45:37 +0800

    :memo: 增加 promise 理解

--- web_FE_100/test.js
+++ web_FE_100/test.js
@@ -8,6 +8,12 @@ const p = new Promise(
 
 
 p.then(2).then(res => console.log('then then', res));
+/**
+ * 问题：为什么第二个then 打印的res 是111不是2呢？
+ * 解释：在这段代码中，p.then(2) 并不会传递 2 给下一个 .then()。
+ * 这是因为 Promise 的规范规定，如果 then() 的第一个参数不是一个函数，
+ * JavaScript 引擎会忽略这个值，并且将其替换为一个默认的函数，这个函数只是简单地返回 Promise 的解决值。
+ */
 
 /**
  * 【问题一】p.then(4)，then 方法里，参数是数值、字符串，（值穿透） 

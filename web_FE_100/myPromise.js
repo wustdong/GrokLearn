4568206b1357fc1e8b3fac402152d6bb82c1ab5b
wustdong <1051987765@qq.com>
2024-10-10 09:43:27 +0800

    :bug: (手写promise) 实现then 的参数合法校验及链式调用；

--- web_FE_100/myPromise.js
+++ web_FE_100/myPromise.js
@@ -3,11 +3,12 @@ const PENDING = 'PENDING';
 const FULFILLED = 'FULFILLED';
 const REJECTED = 'REJECTED';
 
-class Promise {
+class MyPromise {
   constructor(executor) {
     this.status = PENDING;
-    this.value = undefined;
-    this.reason = undefined;
+    // this.value = undefined;
+    // this.reason = undefined;
+    this.promiseResult = undefined;
     // 存放成功的回调
     this.onResolvedCallbacks = [];
     // 存放失败的回调
@@ -16,7 +17,8 @@ class Promise {
     let resolve = (value) => {
       if(this.status ===  PENDING) {
         this.status = FULFILLED;
-        this.value = value;
+        // this.value = value;
+        this.promiseResult = value;
         // 依次将对应的函数执行
         this.onResolvedCallbacks.forEach(fn=>fn());
       }
@@ -25,7 +27,8 @@ class Promise {
     let reject = (reason) => {
       if(this.status ===  PENDING) {
         this.status = REJECTED;
-        this.reason = reason;
+        // this.reason = reason;
+        this.promiseResult = reason;
         // 依次将对应的函数执行
         this.onRejectedCallbacks.forEach(fn=>fn());
       }
@@ -39,29 +42,63 @@ class Promise {
   }
 
   then(onFulfilled, onRejected) {
-    if (this.status === FULFILLED) {
-      onFulfilled(this.value)
-    }
+    // console.log('in then-----')
+    // 参数校验，规范参数为函数
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : val => val;
+    onRejected = typeof onRejected === 'function' ? onRejected : reason => {throw reason};
 
-    if (this.status === REJECTED) {
-      onRejected(this.reason)
-    }
+    let thenPromise =  new MyPromise((resolve, reject) => {
 
-    if (this.status === PENDING) {
-      // 如果promise的状态是 pending，需要将 onFulfilled 和 onRejected 函数存放起来，等待状态确定后，再依次将对应的函数执行
-      this.onResolvedCallbacks.push(() => {
-        onFulfilled(this.value)
-      });
+      // 链式调用核心逻辑
+      let resolvePromise = cb => {
+        try {
+          let x = cb(this.promiseResult);
+          // console.log('x---', x)
+          if (typeof x === MyPromise) {
+            x.then(resolve, reject);
+          } else {
+            resolve(x);
+          }
+        } catch (err) {
+          reject(err);
+        }
+        
+      }
+      if (this.status === FULFILLED) {
+        // onFulfilled(this.value)
+        resolvePromise(onFulfilled);
+      }
 
-      // 如果promise的状态是 pending，需要将 onFulfilled 和 onRejected 函数存放起来，等待状态确定后，再依次将对应的函数执行
-      this.onRejectedCallbacks.push(()=> {
-        onRejected(this.reason);
-      })
-    }
+      if (this.status === REJECTED) {
+        // onRejected(this.reason)
+        resolvePromise(onRejected);
+      }
+
+      if (this.status === PENDING) {
+        // 如果promise的状态是 pending，需要将 onFulfilled 和 onRejected 函数存放起来，等待状态确定后，再依次将对应的函数执行
+        this.onResolvedCallbacks.push(() => {
+          onFulfilled(this.promiseResult)
+        });
+
+        // 如果promise的状态是 pending，需要将 onFulfilled 和 onRejected 函数存放起来，等待状态确定后，再依次将对应的函数执行
+        this.onRejectedCallbacks.push(()=> {
+          onRejected(this.promiseResult);
+        })
+      }
+    });
+    
+    return thenPromise;
+    
   }
 }
 
-// const promise1 = new Promise((resolve, reject) => {
+// 【测试用例 003】
+const promise3 = new MyPromise((resolve, reject) => {
+  console.log('promise exectuor')
+  resolve('成功');
+}).then(22).then(res => console.log('promise then then res:', res))
+// // 【测试用例 001】
+// const promise1 = new MyPromise((resolve, reject) => {
 //     resolve('成功');
 //   }).then(
 //     (data) => {
@@ -73,21 +110,32 @@ class Promise {
 //   )
 
 
-  const promise2 = new Promise((resolve, reject) => {
-    // 传入一个异步操作
-    setTimeout(() => { 
-      resolve('成功');
-    },1000);
-  }).then( // 因为是在settimeout 中调用的，一开始then 时，还没有resolve, then 中的state 是pending 状态
-    (data) => {
-      console.log('success', data) // 此时无返回
-    },
-    (err) => {
-      console.log('faild', err)
-    }
-  )
+// // 【测试用例 002】
+//   const promise2 = new MyPromise((resolve, reject) => {
+//     // 传入一个异步操作
+//     setTimeout(() => { 
+//       resolve('成功');
+//     },1000);
+//   }).then( // 因为是在settimeout 中调用的，一开始then 时，还没有resolve, then 中的state 是pending 状态
+//     (data) => {
+//       console.log('success', data) // 此时无返回
+//     },
+//     (err) => {
+//       console.log('faild', err)
+//     }
+//   )
     
+
+
+
+
+
+
+
+
+
 /**
+ * 参考文章
  * https://juejin.cn/post/6994594642280857630
  * 
  * https://juejin.cn/post/6850037281206566919

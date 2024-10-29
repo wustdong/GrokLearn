738c3a585d6437076439ff44970a5dba3c4d9985
wustdong <1051987765@qq.com>
2024-10-29 13:46:16 +0800

    :tada:(二分查找): 数组算法入门，二分查找

--- leetCode/Array/binarySearch.js
+++ leetCode/Array/binarySearch.js
@@ -1,4 +1,32 @@
 /** leetcode
  * 二分查找只对有序数组
- * 
-*/
+ * 【理解循环不变量（Loop Invariant）】
+ * 【测试】百万级数量时的时间
+*/  
+
+function binarySearch(arr, n, target) {
+    let l = 0,
+        r = n - 1;
+
+    while(l <= r) { // [l, r]区间中查找
+        let mid = Math.round((l + r) / 2);
+        console.log('mid', mid)
+        if (target == arr[mid]) return mid;
+
+        if(target > arr[mid]) {
+            l = mid + 1;
+        } else if(target < arr[mid]) {
+            r = mid - 1;
+        } 
+    }
+
+    return -1;
+}
+
+// 测试用例
+const arr = [1,2,3,5,7,9];
+const target = 0;
+const target2 = 3
+const r = binarySearch(arr, arr.length, target);
+console.log('r:', r)
+

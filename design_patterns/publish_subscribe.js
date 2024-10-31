98df828c438d41bd95d2761a7925fc5a7a6761c9
wustdong <1051987765@qq.com>
2024-10-31 22:51:58 +0800

    :construction:(发布订阅)安装事件调试暂存;

--- design_patterns/publish_subscribe.js
+++ design_patterns/publish_subscribe.js
@@ -14,9 +14,41 @@ var event = {
             return false;
         }
 
-        for(var i = 0; i < fns.length;i++) {
+        for(var i = 0, fn; i < fns.length;i++) {
+            fn = fns[i+1];
             fn.apply(this,arguments);
         }
     }
 
 };
+
+// 赋予对象事件（event）能力
+var eventObj = (function() {
+    let newEventObj = {};
+    for(var key in event) {
+        newEventObj[key] = event[key];
+    }
+    // // 拷贝 event 的属性和方法
+    // for (let key in event) {
+    //     if (typeof event[key] === 'function') {
+    //         // 绑定正确的上下文，使方法可以使用 newEventObj 作为 this
+    //         newEventObj[key] = event[key].bind(newEventObj);
+    //     } else {
+    //         newEventObj[key] = event[key];
+    //     }
+    // }
+    return newEventObj
+})();
+console.log('event', event)
+console.log('eventObj', JSON.stringify(eventObj))
+
+var installEvent = function(obj) {
+    for (var i in event) {
+        obj[i] = event[i];
+    }
+}
+var salesOffices = {}
+var sales = installEvent(salesOffices);
+
+console.log('sales', sales)
+console.log('salesOffices', salesOffices)

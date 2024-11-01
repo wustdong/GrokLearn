86610a98ae1a9bfbd3d945f759ee65c0e9ee2865
wustdong <1051987765@qq.com>
2024-11-01 11:24:57 +0800

    :bug:(发布订阅): 修正调试错误

--- design_patterns/publish_subscribe.js
+++ design_patterns/publish_subscribe.js
@@ -2,45 +2,26 @@ var event = {
     clientList: {},
     listen: function(key,fn) {
         if(!this.clientList[key]) {
-            this.clientList[key] = fn;
+            this.clientList[key] = [];
         }
         this.clientList[key].push(fn);
     },
     trigger: function() {
         var key = Array.prototype.shift.call(arguments);
-        fns = this.clientList[key];
+        var fns = this.clientList[key];
 
         if(!fns || fns.length === 0) {
             return false;
         }
 
         for(var i = 0, fn; i < fns.length;i++) {
-            fn = fns[i+1];
+            fn = fns[i];
             fn.apply(this,arguments);
         }
     }
 
 };
 
-// 赋予对象事件（event）能力
-var eventObj = (function() {
-    let newEventObj = {};
-    for(var key in event) {
-        newEventObj[key] = event[key];
-    }
-    // // 拷贝 event 的属性和方法
-    // for (let key in event) {
-    //     if (typeof event[key] === 'function') {
-    //         // 绑定正确的上下文，使方法可以使用 newEventObj 作为 this
-    //         newEventObj[key] = event[key].bind(newEventObj);
-    //     } else {
-    //         newEventObj[key] = event[key];
-    //     }
-    // }
-    return newEventObj
-})();
-console.log('event', event)
-console.log('eventObj', JSON.stringify(eventObj))
 
 var installEvent = function(obj) {
     for (var i in event) {
@@ -48,7 +29,39 @@ var installEvent = function(obj) {
     }
 }
 var salesOffices = {}
-var sales = installEvent(salesOffices);
-
-console.log('sales', sales)
+installEvent(salesOffices);
 console.log('salesOffices', salesOffices)
+salesOffices.listen('squre90', function() {
+    console.log('99方放盘11')
+})
+salesOffices.listen('squre90', function() {
+    console.log('99方放盘22')
+})
+salesOffices.trigger('squre90')
+
+/**
+ * 类比已知知识 document.body.listen
+ * document.body.addEventListener('click', function () {
+ * // doSomething
+ * })
+ *  document.body.trigger
+ * document.body.click();
+ */
+
+
+// 赋予对象事件（event）能力
+/**
+ * 匿名函数不好复用 
+ */
+// var eventObj = (function() {
+//     let newEventObj = {};
+//     for(var key in event) {
+//         newEventObj[key] = event[key];
+//     }
+    
+//     console.log('newEventObj', newEventObj);
+    
+//     return newEventObj
+// })();
+// console.log('event', event)
+// console.log('eventObj', eventObj)

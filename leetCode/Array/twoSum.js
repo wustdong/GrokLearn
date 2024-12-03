6a00ec89b9be65ee3ce478df12b08de7e786314e
wustdong <1051987765@qq.com>
2024-12-03 17:08:50 +0800

    :construction:使用对撞指针求两数之和；

--- leetCode/Array/twoSum.js
+++ leetCode/Array/twoSum.js
@@ -4,36 +4,17 @@
  * leetcode 167  Two Sum II-Input array is sorted
  */
 
-function search(nums, restTarget) {
-    let i = 0;
-    let j = nums.length - 1;
 
-    while(i <= j) {
-        let mid = Math.floor((i + j ) /2);
-        if(nums[mid] > restTarget) {
-            j = mid - 1;
-        } else if(nums[mid] < restTarget) {
-            i = mid + 1;
-        } else {
-            return mid;
-        }
-    }
-
-    return -1;
-
-}
 function Solution(nums, target) {
-
-    for(let i = 0; i < nums.length; i++) {
-        let a = nums[i];
-        let anotherIndex = search(nums, target - a);
-        console.log('anotherIndex', anotherIndex, i)
-        if( anotherIndex !== -1 && anotherIndex !== i)  {
-            console.log('solution: ', i+1, anotherIndex+1);
-            if (i > anotherIndex) {
-                return [anotherIndex + 1, i +1]
-            }
-            return [i + 1, anotherIndex + 1];
+    let i = 0, j = nums.length - 1;
+    while (i < j) { // 寻找两个不同的元素,所以这里的i 不能等于j
+        if(nums[i] + nums[j] > target) {
+            j--;
+        } else if(nums[i] + nums[j] < target) {
+            i++;
+        } else {
+            console.log('solution: ', [i + 1, j +1])
+            return [i + 1, j +1]
         }
     }
 

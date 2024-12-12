cd34869f25e03f63e06399df6e8f01c793212613
wustdong <1051987765@qq.com>
2024-12-12 11:48:10 +0800

    :bug:修复提交代码到leetcode 报错问题；

--- leetCode/Tree/invertTree.js
+++ leetCode/Tree/invertTree.js
@@ -8,7 +8,15 @@ function invertTree(root) {
     invertTree(root.left);
     invertTree(root.right);
 
+    /**
+     * 为何可以省略变量保存？
+     * 递归调用 invertTree(root.left) 和 invertTree(root.right) 的作用是翻转左右子树：
+     * 递归过程中，root.left 和 root.right 的引用会被直接更新。
+     * 在 [root.left, root.right] = [root.right, root.left] 的交换操作中，你只关心当前节点的子树引用。
+     */
     [root.left, root.right] = [root.right, root.left];
+
+    return root //将当前节点 root 返回，供上层递归调用使用
 }
 
 const root = {
@@ -35,3 +43,5 @@ const root = {
 invertTree(root);
 
 console.log(root)
+
+// leetcode 中的输入是【层序遍历】后得到的【数组】
